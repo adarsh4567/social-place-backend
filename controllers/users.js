15d7b66f8bf83a3718eb05acc0fc77f9b8ac8e24
Adarsh <adarsh800251@gmail.com>
2023-02-24 00:08:49 +0530

    bugs solved

--- controllers/users.js
+++ controllers/users.js
@@ -61,4 +61,9 @@ const addRemoveFriend = async(req,res)=>{
 
 }
 
-module.exports = {getUser,getUserFriends,addRemoveFriend}
+const getAllUser = async(req,res)=>{
+    const user = await User.find({});
+    res.status(200).json(user);
+}
+
+module.exports = {getUser,getUserFriends,addRemoveFriend,getAllUser}
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,14 @@ const express = require('express')
 const {
     getUser,
     getUserFriends,
-    addRemoveFriend
+    addRemoveFriend,
+    getAllUser
 } = require('../controllers/users')
 const verifyToken = require('../middlewares/auth')
 
 const router = express.Router();
 
-
+router.get("/getall",getAllUser);
 router.get("/:id",verifyToken,getUser)
 router.get("/:id/friends",verifyToken,getUserFriends)
 
